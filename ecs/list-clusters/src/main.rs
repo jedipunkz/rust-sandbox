6d456996b3b0c2863027757491f4b75568581c0b
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2022-05-07 23:26:44 +0900

    use clap for cli framework

--- ecs/list-clusters/src/main.rs
+++ ecs/list-clusters/src/main.rs
@@ -1,20 +1,14 @@
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_ecs::{Error, Region, PKG_VERSION};
-use structopt::StructOpt;
+use aws_sdk_ecs::{Error, Region};
+use clap::Parser;
 
-#[derive(Debug, StructOpt)]
-struct Opt {
-    /// The AWS Region.
-    #[structopt(short, long)]
-    region: Option<String>,
-
-    /// Whether to display additional information.
-    #[structopt(short, long)]
-    verbose: bool,
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    region: String,
 }
 
-// List your clusters.
-// snippet-start:[ecs.rust.describe-clusters]
 async fn show_clusters(client: &aws_sdk_ecs::Client) -> Result<(), aws_sdk_ecs::Error> {
     // let resp = client.describe_clusters().send().await?;
     let resp = client.list_clusters().send().await?;
@@ -28,36 +22,14 @@ async fn show_clusters(client: &aws_sdk_ecs::Client) -> Result<(), aws_sdk_ecs::
     }
     Ok(())
 }
-// snippet-end:[ecs.rust.describe-clusters]
 
-/// Lists your Amazon Elastic Container Service clusters in the Region.
-/// # Arguments
-///
-/// * `[-r REGION]` - The Region in which the client is created.
-///   If not supplied, uses the value of the **AWS_REGION** environment variable.
-///   If the environment variable is not set, defaults to **us-west-2**.
-/// * `[-v]` - Whether to display additional information.
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let Opt { region, verbose } = Opt::from_args();
+    let args = Args::parse();
 
-    if verbose {
-        tracing_subscriber::fmt::init();
-    }
-
-    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+    let region_provider = RegionProviderChain::first_try(Region::new(args.region))
         .or_default_provider()
         .or_else(Region::new("us-west-2"));
-
-    if verbose {
-        println!();
-        println!("ECS client version: {}", PKG_VERSION);
-        println!(
-            "Region:             {}",
-            region_provider.region().await.unwrap().as_ref()
-        );
-        println!();
-    }
 
     let shared_config = aws_config::from_env().region(region_provider).load().await;
     let client = aws_sdk_ecs::Client::new(&shared_config);
